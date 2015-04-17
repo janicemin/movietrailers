1e75c011e2cb973b4e51eb093d2f4ea19549ce89
theeltahir <theeltahir@gmail.com>
2015-04-17 12:24:34 -0400

    something

---
+++ app/controllers/actors_controller.rb
@@ -0,0 +1,40 @@
+class ActorsController < ApplicationController
+
+	def index
+
+	end
+	def show
+
+	end
+
+	def edit 
+
+	end
+
+	def update
+
+	end
+
+	def new 
+
+	end
+
+	def create
+
+	end
+
+	def destroy
+
+	end
+
+	private
+
+	def actor_params
+
+	end
+
+	def find_actor
+
+	end
+
+end
